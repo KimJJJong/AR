0e78e20717eb9380c2fa5907f1ff65fc82618ea2
Minjeong Kim <151487549+Mj-K01@users.noreply.github.com>
2024-11-13 06:22:28 +0900

    갤러리 구현

--- Assets/Script/GalleryHandler.cs
+++ Assets/Script/GalleryHandler.cs
@@ -6,14 +6,21 @@ public class GalleryHandler : MonoBehaviour
 {
     public GameObject galleryPanel;
     public GameObject imagePrefab;
+    public GameObject FullImgPanel; 
+    public Image fullScreenImage; 
 
     private void Start()
     {
-        LoadGalleryImages();
+        FullImgPanel.SetActive(false);
     }
 
     public void LoadGalleryImages()
     {
+        foreach (Transform child in galleryPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
         string[] files = Directory.GetFiles(Application.persistentDataPath, "myAR_screenshot_*.png");
 
         foreach (string file in files)
@@ -31,5 +38,36 @@ public class GalleryHandler : MonoBehaviour
         Texture2D texture = new Texture2D(2, 2);
         texture.LoadImage(fileData);
         return texture;
+    }
+
+    public void AddImageToGallery(Texture2D imageTexture)
+    {
+        GameObject newImageObject = Instantiate(imagePrefab, galleryPanel.transform);
+
+        RawImage imageComponent = newImageObject.GetComponent<RawImage>();
+        imageComponent.texture = imageTexture;
+
+        Button imageButton = newImageObject.GetComponent<Button>();
+    
+        if (imageButton != null)
+        {
+            imageButton.onClick.AddListener(() => {
+                Debug.Log("Image clicked");
+                ShowFullScreenImage(imageTexture);
+            });
+            imageButton.onClick.AddListener(() => ShowFullScreenImage(imageTexture));
+        }
+    }
+
+
+    public void ShowFullScreenImage(Texture2D imageTexture)
+    {
+        fullScreenImage.material.mainTexture = imageTexture; // 클릭한 이미지로 설정
+        FullImgPanel.SetActive(true);
+    }
+
+    public void CloseFullScreenImage()
+    {
+        FullImgPanel.SetActive(false);
     }
 }
