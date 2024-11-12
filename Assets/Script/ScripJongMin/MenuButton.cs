aa0ec0cc6ef00caaa658e8e3a1628a5292f0a3c8
KimJJJong <122371707+KimJJJong@users.noreply.github.com>
2024-11-12 23:23:19 +0900

    메뉴
    + menu별 panel 추가

---
+++ Assets/Script/ScripJongMin/MenuButton.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject[] MenuPanle;
+
+
+    public Button[] myButton;   //1 : home      // 2 : Mission  //  3 : ดÞทย  //  4 : PetLv
+    private void Start()
+    {
+        myButton = GetComponentsInChildren<Button>();
+
+        foreach(var panel in MenuPanle)
+            panel.gameObject.SetActive(false);
+
+        myButton[0].onClick.AddListener(()=> MyHomeButton());
+        myButton[1].onClick.AddListener(() => MissionButton());
+        myButton[2].onClick.AddListener(() => GrowthDiaryButton());
+        myButton[3].onClick.AddListener(() => PetLvButton());
+    }
+
+    public void MyHomeButton()
+    {
+        MyHome myHome= myButton[0].gameObject.GetComponent<MyHome>();
+        MenuPanle[0].gameObject.SetActive(true);
+
+    }
+
+    public void MissionButton()
+    {
+        Mission mission = myButton[1].gameObject.GetComponent<Mission>();
+        MenuPanle[1].gameObject.SetActive(true);
+
+    }
+
+    public void GrowthDiaryButton()
+    {
+        GrowthDiary growDiary = myButton[2].gameObject.GetComponent<GrowthDiary>();
+        MenuPanle[2].gameObject.SetActive(true);
+
+    }
+
+    public void PetLvButton()
+    {
+        PetLv petLv = myButton[3].gameObject.GetComponent<PetLv>();
+        MenuPanle[3].gameObject.SetActive(true);
+
+    }
+
+
+
+
+
+}
